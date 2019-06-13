9903fb15929727dd58e787978e3accbbeb7e461d
Philip Sterling <philipssterlings@gmail.com>
2019-06-13 15:22:58 -0700

    bigdecimal

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,24 +1,27 @@
 class Pokemon
 
-    attr_accessor :name, :id, :type, :db
+    attr_accessor :name, :id, :type, :db, :hp
   
-    def initialize(db, name = nil, type = nil, id = nil)
+    def initialize(db, name = nil, type = nil, id = nil, hp = nil)
       @db = db
       @name = name
       @type = type
       @id = id
-      # @hp = hp
+      @hp = hp
     end
   
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?);", name, type)
   end
-  
-  
   
   def self.find(id, db)
-    arr = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    arr = db.execute("SELECT * FROM pokemon WHERE id = ?;", id)
     array = arr.flatten
-    Pokemon.new(db, array[1], array[2], array[0])
+    Pokemon.new(db, array[1], array[2], array[0], array[-1])
   end
+
+  def alter_hp(newhp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?;", newhp , self.id)
+  end
+
 end
